974454513916e642569b734c7218132f98b08df5
DABekiarski <dimitar.bekiarski@gmail.com>
2025-09-24 10:44:59 +0300

    Add files via upload

---
+++ 23.09.2025/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "<h2>My First Page</h2>";
+echo "<br>";
+echo "My Name is ... I am a student in NTBG.<br>";
+echo "I am ... years old.";
+?>
+
+</body>
+</html>
