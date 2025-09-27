ebf2ec2a458d7cc9d96d375382260dcf8ccd68cd
DABekiarski <dimitar.bekiarski@gmail.com>
2025-09-27 08:20:09 +0300

    Add files via upload

---
+++ 25.09.2025/var.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+$var = "Ivan";//Променлива
+//var, която съхранява текста Dimitar
+echo "<h1>Page</h1>";//Изписва Page
+print "<p>My name is $var.</p>";// Изписва My name is 
+?>
+
+
+</body>
+</html>
