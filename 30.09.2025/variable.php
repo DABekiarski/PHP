04746e973ce01a07483cfa5e70b0806839dafaaa
DABekiarski <dimitar.bekiarski@gmail.com>
2025-09-30 17:03:24 +0300

    Add files via upload

---
+++ 30.09.2025/variable.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php
+$name = "Dimitar";//Променлива, съд. "Dimitar"
+$age = 25;//Променлива, съд. числото 25
+$height = 1.80;//Променлива, съд. цифрата 1.80
+echo "My page";//Заглавие на страница
+echo "<p>My name is $name. I am $age years old.</p>";
+echo "<p>I am $height m tall.</p>";
+?>
+
+</body>
+</html>
