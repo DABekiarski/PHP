4d7053ef41c31f451ab23cb5e0d5b3e5803252a1
DABekiarski <dimitar.bekiarski@gmail.com>
2025-11-20 13:45:00 +0200

    Add files via upload

---
+++ 20.11.2025/year2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+<html>
+<head>
+
+</head>
+<body>
+
+
+<?php
+
+$year = 2026;//Променлива year със стойност 2025
+
+//Условна конструкция if-else
+if($year == 2025){
+	//ако year = 2025
+	echo "The year is $year";
+}ELSE {
+	ECHO "The year is not 2025";
+	//echo = ECHO, PHP is not case-sensitive
+}
+
+?>
+
+</body>
+<footer>
+
+</footer>
+</html>
