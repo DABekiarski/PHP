ecd605822ae43af46752ae796437f27734372a74
DABekiarski <dimitar.bekiarski@gmail.com>
2025-11-27 13:46:04 +0200

    Add files via upload

---
+++ 27.11.2025/varcon.php
@@ -0,0 +1,29 @@
+
+
+<?php
+//A-Z,a-z,'_'
+//PHP is case-senstive
+//$age != $AGE
+//$_age VALID
+//$9age NOT VALID
+
+//Константа с име MAX 100
+//const MAX = 100;
+
+//const MAX = 90;
+
+$age = 19;
+//integer, float - 3.14,
+//boolean - true/false,
+//char - %,&,*
+//string - text
+$nameofschool = "NTBG";
+const NAME = "Dimitar";
+const FAMILY = "Bekiarski";
+
+echo "My name is ".NAME." ".FAMILY.".";
+echo "<br>";
+echo "I am ".$age." years old.";
+echo "<br>";
+echo "I am a student in ".$nameofschool.".";
+?>
