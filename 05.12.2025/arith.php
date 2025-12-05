386532df11555702b4ff78ae59bd2d00ea337171
DABekiarski <dimitar.bekiarski@gmail.com>
2025-12-05 16:30:36 +0200

    Add files via upload

---
+++ 05.12.2025/arith.php
@@ -0,0 +1,15 @@
+<?php
+$x = 12.4; // 10
+$y = 3.3;// 3
+
+echo "Add: " . ($x+$y) . "\n";
+echo "Subs: " . ($x-$y) . "\n";
+echo "Div: " . ($x/$y) . "\n";
+echo "Mult: " . ($x * $y) . "\n";
+echo "Modul: " . ($x % $y) . "\n";
+echo "Exp: " . ($x ** $y) . "\n";
+echo "<br>";
+$z = ($x+$y) / 18 + ($x-$y) * 3;
+
+echo "z is " . $z . "<br>";
+?>
