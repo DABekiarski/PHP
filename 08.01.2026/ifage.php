5d238436bb54fedd84178d3b499f1f26d9aef2f0
DABekiarski <dimitar.bekiarski@gmail.com>
2026-01-08 13:44:03 +0200

    Add files via upload

---
+++ 08.01.2026/ifage.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = 16;
+/*
+if($age < 18){//Условие, Condition
+	echo "Minor.";
+	echo "<br>";
+} else {
+	echo "Adult.";
+	echo "<br>";
+}
+*/
+//ternary operator ?
+echo ($age < 18) ? "Minor.":"Adult.";
+					//True  False
+?>
