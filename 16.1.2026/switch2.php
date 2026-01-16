1ba32ad29262a9d705349ad289b5d4fd7a3db7e8
DABekiarski <dimitar.bekiarski@gmail.com>
2026-01-16 17:55:54 +0200

    Add files via upload

---
+++ 16.1.2026/switch2.php
@@ -0,0 +1,18 @@
+<?php
+
+$day = 1;//Day of the week 1-7
+
+switch($day){
+	
+	case 1:
+		echo "Today is Monday.";
+		break;//Stop switch
+	case 2:
+		echo "Today is Tuesday.";
+		break;
+	default:
+		echo "Wrong number of the day.";
+		break;
+}
+
+?>
