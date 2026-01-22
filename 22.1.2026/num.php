cb27a1aeec267c7f6d6b565f87e298b65d5de91f
DABekiarski <dimitar.bekiarski@gmail.com>
2026-01-22 13:42:31 +0200

    Add files via upload

---
+++ 22.1.2026/num.php
@@ -0,0 +1,11 @@
+<?php
+
+for($k = 1; $k <= 10; $k++){//1.Variable k
+						//2.Condition,true/false
+						//3. Actualisation of the variable
+				// k++ = k+1
+	//Body of the loop
+	echo "$k <br>";//10 times
+}
+
+?>
