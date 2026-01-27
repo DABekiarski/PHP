f56aef575ec8ac486b3b72f96d8efb93516571b2
DABekiarski <dimitar.bekiarski@gmail.com>
2026-01-27 14:25:38 +0200

    Add files via upload

---
+++ 27.1.2026/2x1.php
@@ -0,0 +1,13 @@
+<?php
+// 2 x 1 = 2
+// 2 x 2 = 4
+//.....
+
+$number = 2;
+
+for($i = 1; $i <= 10; $i++){
+	echo "$number x $i =  ". $number * $i. "<br>";
+	
+}
+
+?>
---
+++ 27.1.2026/power2.php
@@ -0,0 +1,12 @@
+<?php
+//2 powers
+
+for($i = 2; $i<=512; $i = $i * 2){//1. Initialisation
+					//2. Condition
+					//3.Actualisation
+					//i = i * 2
+		echo $i, "<br>"; 
+}
+
+
+?>
