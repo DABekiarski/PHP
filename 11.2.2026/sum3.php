d006e94773e7af7ec19edbe6d8ccaf71738a4657
DABekiarski <dimitar.bekiarski@gmail.com>
2026-02-11 10:44:38 +0200

    Add files via upload

---
+++ 11.2.2026/sum3.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$number = 5;
+$sum = 1;
+
+for($i = 1;$i <= $number;++$i){//1.Initialisation
+			//2. Condition
+			//3. Actualisation
+	$sum = $sum*$i;
+}
+
+echo "The sum is " . $sum;
+
+?>
