3353ba09bb82545024c2fc352eebe8e1920b1a62
DABekiarski <dimitar.bekiarski@gmail.com>
2026-04-01 09:55:09 +0300

    Add files via upload

---
+++ 1.4.2026/arrayfruits.php
@@ -0,0 +1,19 @@
+<?php
+
+$fruits = array ("apples","pineapples","melons");
+
+//Indexing
+echo "I like to eat ".$fruits[0];
+
+//Associative array
+$person = [
+		"name" => "Alex",
+		"age" => "23",
+		"birthplace" => "Sofia"];
+
+echo "<br>";
+echo "My name is ".$person["name"];
+echo "<br>";
+echo "I am ".$person["age"]. " years old.";
+
+?>
---
+++ 1.4.2026/arraynumbers.php
@@ -0,0 +1,18 @@
+<?php
+
+//New array with 6 elements
+$numbers = array (2,4,8,12,14,18);
+$cars = array("Volvo", "Opel", "Mercedes-Benz");
+
+
+//Indexing
+echo $numbers[0];// First element - 2
+echo "<br>";
+echo $numbers[1];// Second element - 4
+echo "<br>";
+echo $numbers[2];// Third element - 8
+
+echo $cars[0];
+echo "<br>";
+echo $cars[2];
+?>
