06478d2cb1603c00f9e5cba106f20846b49a10a5
DABekiarski <dimitar.bekiarski@gmail.com>
2026-05-27 09:31:42 +0300

    Add files via upload

---
+++ 27.5.2026/sortarray.php
@@ -0,0 +1,16 @@
+<?php
+//Sorting or arranging of elements
+
+//Unsorted array
+$numbers = array(23,4,68,2);
+
+//Sorting
+//sort($numbers);// Ascending
+
+rsort($numbers);//Descending
+//rsort = reverse sort
+
+
+print_r($numbers);
+
+?>
