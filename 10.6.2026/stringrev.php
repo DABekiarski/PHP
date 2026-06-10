886f74567cea31433212617839513ac8715ab1d3
DABekiarski <dimitar.bekiarski@gmail.com>
2026-06-10 09:33:45 +0300

    Add files via upload

---
+++ 10.6.2026/stringrev.php
@@ -0,0 +1,13 @@
+<?php
+
+$stringHello = "Hello";
+$stringDay = "Day!";
+$stringHelloDay = "$stringHello $stringDay";
+
+//echo strrev($stringHello);//Reversed
+//echo str_replace("Day", "Dimitar", $stringHello);
+
+// Concatenate
+//echo $stringHello . " " . $stringDay;
+echo $stringHelloDay;
+?>
